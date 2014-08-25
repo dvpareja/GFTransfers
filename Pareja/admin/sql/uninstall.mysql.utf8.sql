00969d2a9145a3b7f17074eb7d9bf0c1d51e33b2
darth <darth@laptop.darth.es>
2014-08-25 17:44:51 +0200

    uninstall mysql

--- Pareja/admin/sql/uninstall.mysql.utf8.sql
+++ Pareja/admin/sql/uninstall.mysql.utf8.sql
@@ -1,1 +1,4 @@
-DROP TABLE IF EXISTS #__gftransfers;
+DROP TABLE IF EXISTS #__gftransfers_countries;
+DROP TABLE IF EXISTS #__gftransfers_states;
+DROP TABLE IF EXISTS #__gftransfers_zones;
+DROP TABLE IF EXISTS #__gftransfers_currencies;
